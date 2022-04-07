b7643eb5ccab4ed72dfb1ab70b73ffbfe7688367
pytera895143242 <91798465+pytera895143242@users.noreply.github.com>
2022-04-07 17:53:37 +0500

    Add files via upload

---
+++ misc.py
@@ -0,0 +1,13 @@
+import logging
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+
+
+TOKEN = '5210768526:AAHH-mFO6csY3DGbGNDP5EXL6EFZ1jgvbiQ' #Токен бота
+memory_storage = MemoryStorage()
+
+bot = Bot(token=TOKEN,parse_mode='html')
+dp = Dispatcher(bot,storage=memory_storage)
+logging.basicConfig(level=logging.INFO)
